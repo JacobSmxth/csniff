a988bc20e7b522d85ecc735ae8b2eb7b193ab75e
Jacob Smith <jacob.d.smith@live.com>
2026-03-11 18:40:21 -0400

    Create the logic for the get_ports, and get_target functions.

--- src/parsing.c
+++ src/parsing.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,39 +17,76 @@ char validate_argument(char *argument) {
   return argument[1];
 }
 
-void two_args(char *argv[]) {
-  char choice = validate_argument(argv[1]);
-  if (choice == '0') {
-    fprintf(stderr, "unknown thingy\n");
-    return;
-  }
-
-  printf("%c\n", choice);
-
-  switch (choice) {
-  case 'h':
-    printf("HELP PAGE\nTHERE IS NO HELP YET\n");
-    break;
-  case 'm':
-    printf("m is for 'mine'. This will evenutally scan your network for all "
-           "open ports\n");
-    break;
-  default:
-    printf("oh no\n");
-    break;
-  }
-}
-
 void print_help() {
   printf("HELP ISN't AVAILABLE... Trying API again in 5 seconds... (I'm joking "
          "just haven't coded anything\n");
 }
 
-void get_ports(ScanArgs *args, char *ports) {
+int *validate_port_range(char *ports) {
+  int start, end;
+  int matched = sscanf(ports, "%d-%d", &start, &end);
+
+  int *port = malloc(sizeof(int) * 2);
+  if (matched == 2) {
+    port[0] = start;
+    port[1] = end;
+  } else {
+    matched = sscanf(ports, "%d", &start);
+    if (matched == 1) {
+      port[0] = start;
+      port[1] = start;
+    } else {
+      free(port);
+      port = NULL;
+    }
+  }
+  return port;
+}
+
+int get_ports(ScanArgs *args, char *ports) {
   // Just making sure my pointer logic is correct
   printf("PORTS: %s\n", ports);
-  args->port_end = 100;
-  args->port_start = 11;
+
+  int *thePorts = validate_port_range(ports);
+  if (thePorts) {
+    if (thePorts[0] == thePorts[1]) {
+      printf("Single Port: %d\n", thePorts[0]);
+      args->port_start = thePorts[0];
+      args->port_end = thePorts[0];
+    } else {
+
+      printf("Dual Ports: \n");
+      printf("%d\n", thePorts[0]);
+      printf("%d\n", thePorts[1]);
+      args->port_start = thePorts[0];
+      args->port_end = thePorts[1];
+    }
+    free(thePorts);
+  } else {
+    fprintf(stderr, "Invalid\n");
+    free(thePorts);
+    return 0;
+  }
+  return 1;
+}
+
+int get_target(ScanArgs *scan_args, char *target) {
+  struct in_addr addr;
+  if (!scan_args) {
+    fprintf(stderr, "ScanArgs doesn't exist\n");
+    return 0;
+  }
+
+  if (inet_pton(AF_INET, target, &addr) == 1) {
+    size_t len = strlen(target) + 1;
+    scan_args->target = malloc(len);
+    memcpy(scan_args->target, target, len);
+  } else {
+    fprintf(stderr, "This is an invalid target address\n");
+    return 0;
+  }
+
+  return 1;
 }
 
 void process_parsings(int argc, char *argv[]) {
@@ -58,18 +96,20 @@ void process_parsings(int argc, char *argv[]) {
     return;
   }
 
-  char *target = NULL;
   for (int i = 1; i < argc; i++) {
     char argument = validate_argument(argv[i]);
     switch (argument) {
     case 't':
-      printf("TARGET: %s\n", argv[++i]);
-      scan_args->target = malloc(strlen(argv[i]) + 1);
-      memcpy(scan_args->target, argv[i], strlen(argv[i]) + 1);
+      get_target(scan_args, argv[++i]);
       break;
     case 'p':
-      printf("PORT RANGE: %s\n", argv[++i]);
-      get_ports(scan_args, argv[i]);
+      if (!scan_args->target) {
+        printf("No target specified, falling back to default\n");
+        char *target = "192.168.1.10";
+        scan_args->target = malloc(strlen(target) + 1);
+        memcpy(scan_args->target, target, strlen(target) + 1);
+      }
+      get_ports(scan_args, argv[++i]);
       break;
     case 'h':
       print_help();
@@ -83,15 +123,14 @@ void process_parsings(int argc, char *argv[]) {
               "make up things. im serious. ",
               argv[i]);
       argv[++i] ? printf("You even tried saying it was '%s'. LMAO", argv[i])
-                : printf("");
+                : printf(" ");
       printf("\n");
       exit(EXIT_FAILURE);
     }
-
-    // Making sure the values are being correctly changes
-    printf("%s: %d - %d\n", scan_args->target, scan_args->port_start,
-           scan_args->port_end);
   }
+  printf("FINAL CHECK: %s: %d, %d", scan_args->target, scan_args->port_start,
+         scan_args->port_end);
+  free(scan_args);
 }
 
 int parse_arguments(int argc, char *argv[]) {
