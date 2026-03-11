986fcd52c2119ae9ac4b22485571f876fdb41b2b
Jacob <jacob.d.smith@live.com>
2026-03-11 01:16:19 -0400

    I take back what I was saying. I spent 30 minutes trying to get this simple stuff to work

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "parsing.h"
-#include "prompt.h"
 
 int main(int argc, char *argv[]) {
   parse_arguments(argc, argv);
-  menuPrompt();
   return 0;
 }
--- src/parsing.c
+++ src/parsing.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
-int parse_arguments(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    printf("%d: %s\n", i, argv[i]);
+
+char validate_argument(char *argument) {
+  if (argument[0] != '-') {
+    return '0';
   }
 
-  return argc;
+  return argument[1];
+}
+
+void two_args(char *argv[]) {
+  char choice = validate_argument(argv[1]);
+  if (choice == '0') {
+    fprintf(stderr, "unknown thingy\n");
+    return;
+  }
+
+  printf("%c\n", choice);
+
+  switch (choice) {
+  case 'h':
+    printf("HELP PAGE\nTHERE IS NO HELP YET\n");
+    break;
+  case 'm':
+    printf("m is for 'mine'. This will evenutally scan your network for all "
+           "open ports\n");
+    break;
+  default:
+    printf("oh no\n");
+    break;
+  }
+}
+
+void process_parsings(int argc, char *argv[]) {
+  if (argc <= 1) {
+    fprintf(stderr, "Error: must enter arguments (See -h for help)\n");
+  } else if (argc == 2) {
+    two_args(argv);
+  }
+}
+
+int parse_arguments(int argc, char *argv[]) {
+  process_parsings(argc, argv);
+
+  return 1;
 }
