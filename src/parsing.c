a0b280b71d21d9413c0fc1555e279fa6a8c52dfe
Jacob <jacob.d.smith@live.com>
2026-03-11 01:45:26 -0400

    Made good progress on the parser. Detecting targets and ports, just need to finish the parsing of the ports, and making sure the target is a true IP

--- src/parsing.c
+++ src/parsing.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char *target;
+  int port_start;
+  int port_end;
+} ScanArgs;
 
 char validate_argument(char *argument) {
   if (argument[0] != '-') {
@@ -36,20 +44,53 @@ void print_help() {
          "just haven't coded anything\n");
 }
 
+void get_ports(ScanArgs *args, char *ports) {
+  // Just making sure my pointer logic is correct
+  printf("PORTS: %s\n", ports);
+  args->port_end = 100;
+  args->port_start = 11;
+}
+
 void process_parsings(int argc, char *argv[]) {
+  ScanArgs *scan_args = malloc(sizeof(ScanArgs));
+  if (!scan_args) {
+    fprintf(stderr, "Malloc error\n");
+    return;
+  }
+
+  char *target = NULL;
   for (int i = 1; i < argc; i++) {
     char argument = validate_argument(argv[i]);
     switch (argument) {
     case 't':
       printf("TARGET: %s\n", argv[++i]);
+      scan_args->target = malloc(strlen(argv[i]) + 1);
+      memcpy(scan_args->target, argv[i], strlen(argv[i]) + 1);
       break;
     case 'p':
       printf("PORT RANGE: %s\n", argv[++i]);
+      get_ports(scan_args, argv[i]);
       break;
     case 'h':
       print_help();
       break;
+    case 'm':
+      printf("Hello darling :)\n");
+      break;
+    default:
+      fprintf(stderr,
+              "Did you seriously think that '%s' would work. Stop trying to "
+              "make up things. im serious. ",
+              argv[i]);
+      argv[++i] ? printf("You even tried saying it was '%s'. LMAO", argv[i])
+                : printf("");
+      printf("\n");
+      exit(EXIT_FAILURE);
     }
+
+    // Making sure the values are being correctly changes
+    printf("%s: %d - %d\n", scan_args->target, scan_args->port_start,
+           scan_args->port_end);
   }
 }
 
